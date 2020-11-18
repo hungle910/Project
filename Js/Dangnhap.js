c5919ba077026b4c3aab005cc595018114124f7d
hungle910 <70809735+hungle910@users.noreply.github.com>
2020-11-18 09:55:27 +0700

    Updated 18/11

--- Js/Dangnhap.js
+++ Js/Dangnhap.js
@@ -1,13 +1,19 @@
-Function:SignIn(){
-    Accont_email=Duyhungle140@Admins
-    Account_password=12345678
+account_email = 'admin@admin'
+acount_password = 'password'
+
+function signIn() {
     let email_input = document.getElementById('email').value
-    let password_input = document.getElementById('password').value
-    let caseblock = document.getElementById('Checkcase')
-    if(email_input == Account_email && password_input == Account_password)
-    {Caseblock.innerHTML='Sign in Successful'
-    Caseblock.style.colour='green'}
-    else{
-        caseblock.style.display='block'
+    let password_input = document.getElementById('Password').value;
+    let caseBlock = document.getElementById('checkcase');
+
+    if (email_input == account_email && acount_password == password_input) {
+        caseBlock.style.display = 'block';
+        caseBlock.innerHTML = 'Sign in successful!!!'
+        caseBlock.style.color = 'green'
+    }
+
+    else {
+        caseBlock.style.display = 'block';
+        caseBlock.style.color = 'red';
     }
 }
